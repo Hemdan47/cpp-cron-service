c1a9bc2e9e1d06b01329347485e0509b5f70c079
reham549 <r.2424102002@gmail.com>
2026-06-16 00:19:43 +0300

    feat(interval-job): add next run calculation based on interval_seconds from schedule payload

--- src/domain/IntervalJob.cpp
+++ src/domain/IntervalJob.cpp
@@ -1,10 +1,20 @@
 #include "domain/IntervalJob.h"
+#include <nlohmann/json.hpp>
 
+using json = nlohmann::json;
 
 IntervalJob::IntervalJob(std::string id , std::string name , ScheduleType type , std::string command , JobStatus status , std::string schedule_payload)
     : Job(id , name , type , command , status , schedule_payload)
-{
+
     // IntervalJob::calculate_next_run();
-}
+
 
 IntervalJob::IntervalJob(const JobData &data) : Job(data) {}
+
+std::chrono::sys_seconds IntervalJob::calculate_next_run(std::chrono::sys_seconds currentTime) {
+    json payload = json::parse(this->schedule_payload);
+
+    int interval_seconds = payload["interval_seconds"];
+
+    return currentTime + std::chrono::seconds(interval_seconds);
+}
