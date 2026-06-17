ee1f66dc0e644df318bd68a6ceed5e37f8971fdf
Hemdan47 <mohammedahmed98731@gmail.com>
2026-06-17 06:29:46 +0300

    refactor: update JobRecoveryService to use consistent naming and improve member variable accessibility and fix the typo in the class name

--- include/service/JobRecoveryService.h
+++ include/service/JobRecoveryService.h
@@ -1,21 +1,22 @@
 #pragma once
-
 #include <memory>
 #include "repository/IJobRepository.h"
-#include "factory/JobFactory.h"
+#include "factory/JopFactory.h"
 #include "scheduler/SchedulerDaemon.h"
 
 class JobRecoveryService {
-private:
-    std::shared_ptr<IJobRepository>   repo;
-    std::shared_ptr<JobFactory>       factory;
-    std::shared_ptr<SchedulerDaemon>  daemon;
-
 public:
     JobRecoveryService(
         std::shared_ptr<IJobRepository> repo,
         std::shared_ptr<JobFactory> factory,
-        std::shared_ptr<SchedulerDaemon> daemon
-    );
+        std::shared_ptr<SchedulerDaemon> daemon);
 
     void execute();
+
+private:
+    std::shared_ptr<IJobRepository>   _repo;
+    std::shared_ptr<JobFactory>       _factory;
+    std::shared_ptr<SchedulerDaemon>  _daemon;
+
+
+};
---
+++ src/service/JobRecoveryService.cpp
@@ -0,0 +1,29 @@
+#include "service/JopRecoveryService.h"
+
+JobRecoveryService::JobRecoveryService(
+    std::shared_ptr<IJobRepository> repo,
+    std::shared_ptr<JobFactory> factory,
+    std::shared_ptr<SchedulerDaemon> daemon
+) : _repo(std::move(repo)), _factory(std::move(factory)), _daemon(std::move(daemon)) {}
+
+void JobRecoveryService::execute() {
+    std::vector<JobData> active_jobs = _repo->find_active();
+
+    auto now = std::chrono::time_point_cast<std::chrono::seconds>(
+        std::chrono::system_clock::now()
+    );
+
+    for (auto& dto : active_jobs) {
+        std::shared_ptr<Job> job = _factory->create_job(dto);
+        const auto stored_next_run= job->get_next_run();
+
+        if (stored_next_run < now) {
+            // calculate_next_run will save the next run internally in the job object
+            const std::chrono::sys_seconds new_next_run = job->calculate_next_run();
+            dto._next_run = new_next_run;
+            _repo->update_job(dto);
+        }
+
+        _daemon->add_job(job);
+    }
+}
