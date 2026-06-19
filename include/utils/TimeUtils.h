f57f8bf6bdf6271206b1b0703e3863e8da660f4a
Hemdan47 <mohammedahmed98731@gmail.com>
2026-06-19 17:15:42 +0300

    implement cron job parsing and calculate next run

--- include/utils/TimeUtils.h
+++ include/utils/TimeUtils.h
@@ -2,4 +2,5 @@
 #include <chrono>
 #include <string>
 
+std::tuple<unsigned, unsigned, int, int, int> sys_seconds_to_calendar(std::chrono::sys_seconds tp);
 std::chrono::sys_seconds iso_to_sys_seconds(const std::string& iso_date);
