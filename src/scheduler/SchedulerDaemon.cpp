14a8a08f3a2ab6e56c80a55bdaad496f7bd3c8f2
Hemdan47 <mohammedahmed98731@gmail.com>
2026-06-19 03:06:07 +0300

    feat: implement SchedulerDaemon with start/stop methods

--- src/scheduler/SchedulerDaemon.cpp
+++ src/scheduler/SchedulerDaemon.cpp
@@ -1,1 +1,17 @@
 #include "scheduler/SchedulerDaemon.h"
+
+SchedulerDaemon::SchedulerDaemon(std::shared_ptr<ThreadPool> thread_pool)
+    : _thread_pool(std::move(thread_pool)), _stop_signal(false){}
+
+void SchedulerDaemon::start() {
+    _thread = std::thread(&SchedulerDaemon::run, this);
+}
+
+void SchedulerDaemon::stop() {
+    _stop_signal = true;
+    _cv.notify_all();
+    if (_thread.joinable()) {
+        _thread.join();
+    }
+}
+
